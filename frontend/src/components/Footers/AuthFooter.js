98783fe6c396c66f22bc728f33230b970dad43c9
Lawal Selim <lawalselim0@gmail.com>
2024-03-17 15:36:51 +0000

    rebuilt the backend structure

---
+++ frontend/src/components/Footers/AuthFooter.js
@@ -0,0 +1,32 @@
+
+/*eslint-disable*/
+
+// reactstrap components
+import { NavItem, NavLink, Nav, Container, Row, Col } from "reactstrap";
+
+const Login = () => {
+  return (
+    <>
+      <footer className="py-5">
+        <Container>
+          <Row className="align-items-center justify-content-xl-between">
+            <Col xl="6">
+              <div className="copyright text-center text-xl-left text-muted">
+                © {new Date().getFullYear()}{" "}
+                <a
+                  className="font-weight-bold ml-1"
+                  href="#"
+                  target="_blank"
+                >
+                  Selim Lawal
+                </a>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
+  );
+};
+
+export default Login;
---
+++ out/production/USSD-Ambulance/README.md
@@ -0,0 +1,2 @@
+# USSD-Ambulance
+This is my masters Dissertation project. The goal is to develop an ambulance booking system that allows people to book for ambulance via the USSD.
